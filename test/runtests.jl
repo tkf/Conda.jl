512831413db0a1a62dadbba167c381f0a323905e
Takafumi Arakaki <aka.tkf@gmail.com>
2018-09-20 02:27:07 -0700

    Test Conda.add for Python package

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 using Conda, Compat, VersionParsing
+using Conda: _set_conda_env, bin_dir
+using Compat
+using Compat: @info
 using Compat.Test
+
+exe = Compat.Sys.iswindows() ? ".exe" : ""
 
 Conda.update()
 
@@ -7,11 +12,21 @@ env = :test_conda_jl
 @test Conda.exists("curl", env)
 Conda.add("curl", env)
 
+@testset "Install Python package" begin
+    Conda.add("python", env)
+    pythonpath = joinpath(Conda.python_dir(env), "python" * exe)
+    @test isfile(pythonpath)
+
+    cmd = _set_conda_env(`$pythonpath -c "import zmq"`, env)
+    @test_throws Exception run(cmd)
+    @test !success(cmd)
+    Conda.add("pyzmq", env)
+    run(cmd)
+end
+
 curlvers = Conda.version("curl",env)
 @test curlvers >= v"5.0"
 @test Conda.exists("curl==$curlvers", env)
-
-exe = Compat.Sys.iswindows() ? ".exe" : ""
 
 curl_path = joinpath(Conda.bin_dir(env), "curl" * exe)
 @test isfile(curl_path)
